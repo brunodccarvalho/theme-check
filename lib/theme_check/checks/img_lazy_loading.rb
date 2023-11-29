1f4854ccd4f7b6d7ba0a122f1360f0f38f7bad6e
Bruno Carvalho <bruno.carvalho.feup@gmail.com>
2023-11-29 13:18:48 +0000

    Check data-src, data-srcset and lazysizes script in ImgLazyLoading

--- lib/theme_check/checks/img_lazy_loading.rb
+++ lib/theme_check/checks/img_lazy_loading.rb
@@ -5,12 +5,20 @@ module ThemeCheck
     categories :html, :performance
     doc docs_url(__FILE__)
 
-    ACCEPTED_LOADING_VALUES = %w[lazy eager]
+    ACCEPTED_LOADING_VALUES = %w[lazy eager].freeze
+    BANNED_ATTRIBUTES = %w[data-src data-srcset].freeze
+    LAZYSIZE_SCRIPT_REGEX = /lazysize/.freeze
 
     def on_img(node)
       loading = node.attributes["loading"]&.downcase
-      return if ACCEPTED_LOADING_VALUES.include?(loading)
-      add_offense("Use loading=\"eager\" for images visible in the viewport on load and loading=\"lazy\" for others", node: node)
+      add_offense("Use loading=\"eager\" for images visible in the viewport on load and loading=\"lazy\" for others", node: node) unless ACCEPTED_LOADING_VALUES.include?(loading)
+      add_offense("Remove data-src and data-srcset attributes", node: node) if BANNED_ATTRIBUTES.any? { |attr| node.attributes.key?(attr) }
+    end
+
+    def on_script(node)
+      return unless node.attributes["src"].to_s.match?(LAZYSIZE_SCRIPT_REGEX)
+
+      add_offense("Remove lazysizes script", node: node)
     end
   end
 end
