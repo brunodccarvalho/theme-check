2addaab7ad51344ecbd4e38565d1e1fead3e9acf
Bruno Carvalho <bruno.carvalho.feup@gmail.com>
2023-12-19 10:02:36 +0000

    Relax img loading attribute value check

--- lib/theme_check/checks/img_lazy_loading.rb
+++ lib/theme_check/checks/img_lazy_loading.rb
@@ -5,13 +5,11 @@ module ThemeCheck
     categories :html, :performance
     doc docs_url(__FILE__)
 
-    ACCEPTED_LOADING_VALUES = %w[lazy eager].freeze
     BANNED_ATTRIBUTES = %w[data-src data-srcset].freeze
     LAZYSIZE_SCRIPT_REGEX = /lazysize/.freeze
 
     def on_img(node)
-      loading = node.attributes["loading"]&.downcase
-      add_offense("Use loading=\"eager\" for images visible in the viewport on load and loading=\"lazy\" for others", node: node) unless ACCEPTED_LOADING_VALUES.include?(loading)
+      add_offense("Use loading=\"eager\" for images visible in the viewport on load and loading=\"lazy\" for others", node: node) unless node.attributes["loading"]
       add_offense("Remove data-src and data-srcset attributes", node: node) if BANNED_ATTRIBUTES.any? { |attr| node.attributes.key?(attr) }
     end
 
